a917a64581cf8223351c0a06577a07b00d3be92d
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2023-05-13 19:31:45 -0400

    [EDIT] Created collapsable menu with mobile responsiveness

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,24 @@
-import React from "react";
-import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/logo.svg";
 import "./navbar.css";
 
+const Menu = () => (
+  <>
+    <p><a href="#home">Home</a></p>
+    <p><a href="#whatgpt3">What is GPT-3?</a></p>
+    <p><a href="#possibility">OpenAI</a></p>
+    <p><a href="#features">Case Studies</a></p>
+    <p><a href="#blog">Library</a></p>
+  </>
+)
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="gpt3__navbar">
+      {/* Container for Nav-Bar Logo & Page Links */}
       <div className="gpt3__navbar-links">
         {/* Logo of Nav-Bar */}
         <div className="gpt3__navbar-links_logo">
@@ -14,12 +27,36 @@ const Navbar = () => {
 
         {/* Page Links of Nav-Bar */}
         <div className="gpt3__navbar-links_container">
-          <p><a href="#home">Home</a></p>
-          <p><a href="#whatgpt3">What is GPT-3?</a></p>
-          <p><a href="#possibility">OpenAI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog">Library</a></p>
+          <Menu />
         </div>
+      </div>
+
+      {/* Container for Nav-Bar Sign In/Up Links */}
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+
+      {/* Container for Nav-Bar Menu Links */}
+      <div className="gpt3__navbar-menu">
+        {/* Collapsed Menu in Nav-Bar Header */}
+        {toggleMenu 
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} /> 
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+        }
+
+        {/* Expanded Menu in Nav-Bar Header */}
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_links">
+              <Menu />
+              <div className="gpt3__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
