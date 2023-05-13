718bc1364338d92b5f394b9d9556d58de737d4bb
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2023-05-13 16:54:13 -0400

    [STYLE] Added some main app comments to start today's development

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+// Importing React library into namespace
 import React from "react";
 
+// Importing custom components and containers
 import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from "./containers";
 import { Brand, CTA, Navbar } from "./components";
 
+// Define main application
 const App = () => {
   return (
     <div className="App">
