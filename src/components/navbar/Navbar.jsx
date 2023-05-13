c29c73c2c9756f0af264c3cb202c2fb47a412061
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2023-05-13 18:49:23 -0400

    [EDIT] Added logo and page access links to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
 import "./navbar.css";
 
 const Navbar = () => {
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
+        {/* Logo of Nav-Bar */}
         <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo" />
+        </div>
+
+        {/* Page Links of Nav-Bar */}
+        <div className="gpt3__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#whatgpt3">What is GPT-3?</a></p>
+          <p><a href="#possibility">OpenAI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
         </div>
       </div>
     </div>
