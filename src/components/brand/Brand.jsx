f2d4646e8a88d6baea3b86f6aea8e8e26301edf3
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2023-05-13 23:33:35 -0400

    [EDIT] Created brand layout under header of main app

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,38 @@
+// General importations for React functionality
 import React from "react";
+// Local instantiation of custom styles
 import "./brand.css";
+// Importing branding images locally
+import { google, slack, atlassian, dropbox, shopify} from "./imports"
 
+// Major component for branding materials
 const Brand = () => {
   return (
-    <div>
-        Brand
+    <div className="gpt3__brand section__padding">
+      {/* Render image for Google */}
+      <div>
+        <img src={google} alt="google" />
+      </div>
+
+      {/* Render image for Slack */}
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+
+      {/* Render image for Atlassian */}
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+
+      {/* Render image for DropBox */}
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+
+      {/* Render image for Shopify */}
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
     </div>
   )
 }
